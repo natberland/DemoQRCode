7767da17d09d30fdd16c9fc16d73dc4439c861d0
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-08 22:52:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/natberland/demoqrcode/LeitorQRCodeActivity.kt
@@ -0,0 +1,90 @@
+package com.example.natberland.demoqrcode
+
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
+import com.google.zxing.Result
+import kotlinx.android.synthetic.main.activity_leitor_qrcode.*
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+
+class LeitorQRCodeActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+    private val REQUEST_CAMERA = 212
+    override fun handleResult(rawResult: Result?) {
+        val codigoVideoNoYoutube = rawResult?.text.toString()
+        mScannerView.resumeCameraPreview(this)
+        val intent = Intent(this, VideoActivity::class.java)
+        intent.putExtra("YOUTUBE_CODE", codigoVideoNoYoutube)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leitor_qrcode)
+        checkPermission()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mScannerView.setResultHandler(this)
+            mScannerView.startCamera()
+        } else {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+            ) {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("Necessária a permissao para Camera")
+                        .setTitle("Permissao Requerida")
+                builder.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, id ->
+                    requestPermission()
+                })
+                val dialog = builder.create()
+                dialog.show()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    protected fun requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CAMERA -> {
+                if (grantResults.size == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permissao negada", Toast.LENGTH_SHORT).show()
+                } else {
+                }
+                return
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/natberland/demoqrcode/VideoActivity.kt
@@ -0,0 +1,34 @@
+package com.example.natberland.demoqrcode
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_video.*
+
+class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    private var youTubePlayer: YouTubePlayer? = null
+    override fun onInitializationSuccess(
+            provider: YouTubePlayer.Provider,
+            player: YouTubePlayer,
+            wasRestored: Boolean) {
+        if (!wasRestored) {
+            youTubePlayer = player
+            youTubePlayer?.setFullscreen(true)
+            youTubePlayer?.setShowFullscreenButton(false)
+            player.cueVideo(intent.getStringExtra("YOUTUBE_CODE")); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+            //player.loadVideo("https://www.youtube.com/watch?v=fhWaJi1Hsfo")
+        }
+    }
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider, errorReason: YouTubeInitializationResult) {
+        val error = String.format("Erro", errorReason.toString())
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+        ytVideo.initialize("AIzaSyAwxen9qrmhS8TFOHZW0gzEc5A3P0CNWwY", this)
+    }
+}
